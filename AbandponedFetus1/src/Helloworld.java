8660fd5ad06a60ec6e2f1893d3157085550e8838
yourmoms-boyfriend <25kvang5883@apps-hcpak12.org>
2022-11-16 19:03:48 -0600

    added java project

---
+++ AbandponedFetus1/src/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {// Create class Helloworld. Hello I'm the hottest babe talking right now
+// inside of these brackets defines the classes scope
+    public static void main(String[] args){// create main method
+
+        System.out.println("What has 128 teeth and is holding back a monster? The zipper to my pants");
+        // printing to console 
+    }
+}
